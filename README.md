d43321e55e4b237bfaec21815f183a2130900be7
nozomi-iida <iida19990106@gmail.com>
2022-07-07 19:58:59 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# React/Rest API BoilerPlate
+## セットアップ手順
+1. `npm i`
+2. `npm run dev`
---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import checker from "vite-plugin-checker";
+import tsconfigPaths from "vite-tsconfig-paths";
+import svgr from "vite-plugin-svgr";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    checker({
+      typescript: true,
+      eslint: {
+        lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+      },
+    }),
+    tsconfigPaths(),
+    svgr(),
+  ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  }
+});
